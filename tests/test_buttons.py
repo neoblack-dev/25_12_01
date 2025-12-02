73344fb2ae716a7490df18f3ce4cc0515230f06e
neoblack-dev <neoblackrussian@gmail.com>
2025-12-02 19:10:20 +0500

    added first test

--- tests/test_buttons.py
+++ tests/test_buttons.py
@@ -1,1 +1,12 @@
-import playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+from time import sleep
+
+def test_button_exists(page: Page):
+    page.goto("https://www.wildberries.ru/catalog/126793555/detail.aspx")
+    sleep(5)
+    button = page.locator('.mo-button mo-button_view_fill mo-button_colors_brand mo-button_width_fill mo-button_size_large')
+    expect.button.to_be_visible()
+
+
+
+
