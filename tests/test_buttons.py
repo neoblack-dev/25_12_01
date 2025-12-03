fb9e89f581696198af4ce2637dc891d939414804
neoblack-dev <neoblackrussian@gmail.com>
2025-12-03 06:21:21 +0500

    redacted first test

--- tests/test_buttons.py
+++ tests/test_buttons.py
@@ -2,10 +2,12 @@ from playwright.sync_api import Page, expect
 from time import sleep
 
 def test_button_exists(page: Page):
-    page.goto("https://www.wildberries.ru/catalog/126793555/detail.aspx")
+    page.goto("https://www.citilink.ru/")
     sleep(5)
-    button = page.locator('.mo-button mo-button_view_fill mo-button_colors_brand mo-button_width_fill mo-button_size_large')
-    expect.button.to_be_visible()
+    button = page.get_by_role("button", name='Создать сборку')
+    expect(button).to_be_visible()
+    page.get_by_role("button", name='Создать сборку').click()
+    sleep(5)
 
 
 
